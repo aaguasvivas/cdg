2b29b8a16ab70ac35103b9941f5a3fb0e99c7aec
Neil Zhao <neil@zzrcxb.me>
2019-07-09 04:38:46 -0500

    can build

--- cdg/cdg.cpp
+++ cdg/cdg.cpp
@@ -15,129 +15,115 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
+using namespace std;
 using namespace llvm;
 
 // enable debugging output
 //#define MK_DEBUG
 
 namespace {
-  // Module pass
-  struct DependenceCheck : public ModulePass {
-    // Default constructor
-    DependenceCheck();
+    // Module pass
+    struct DependenceCheck : public ModulePass {
+        // Default constructor
+        DependenceCheck() : ModulePass(ID) {
+            initializeMemoryDependenceAnalysisPass(*PassRegistry::getPassRegistry());
+        }
 
-    // DataDependence Object. Holds/calculates the data dependency information
-    DataDependence DataDep;
+        // DataDependence Object. Holds/calculates the data dependency information
+        DataDependence DataDep;
 
-    // ControlDependence object. Holds and calculates the control dependency information
-    ControlDependence ControlDep;
+        // ControlDependence object. Holds and calculates the control dependency information
+        ControlDependence ControlDep;
 
-    static char ID; // pass ID
+        static char ID; // pass ID
 
-    virtual bool runOnModule(Module &M);
+        bool runOnModule(Module &M) override {
+            auto &AAWP = getAnalysis<AAResultsWrapperPass>();
+            auto &AA = AAWP.getAAResults();
 
+            // Iterate over all instructions and get the memory dependence information.
+            for (auto &mi : M) {
+                // skip external functions
+                if (mi.isDeclaration())
+                    continue;
 
-    virtual void getAnalysisUsage(AnalysisUsage &AU) const;
-    void print(raw_ostream &OS, const Module *m = 0) const;
+                // Since MemoryDependenceAnalysis is a function pass, we need to pass the
+                // current function we are examining to the getAnalysis() call.
+                auto &MDA = getAnalysis<MemoryDependenceAnalysis>(mi);
+                auto &PDT = getAnalysis<PostDominatorTree>(mi);
 
-  };
+                DataDep.getDataDependencies(mi, MDA, AA);
 
-  bool DependenceCheck::runOnModule(Module &M) {
-#ifdef MK_DEBUG
-    errs() << "[DEBUG] DependenceCheck::runOnModule()\n";
-#endif
+                ControlDep.getControlDependencies(mi, PDT);
 
-    AliasAnalysis &AA = getAnalysis<AliasAnalysis>();
+            } // end for (module::iterator)
 
-    // Iterate over all instructions and get the memory dependence information.
-    for (auto mi = M.begin(); mi != M.end(); ++mi) {
-      // skip external functions
-      if (mi->isDeclaration())
-        continue;
+            // Nothing modified in IR
+            return false;
+        }
 
-      // Since MemoryDependenceAnalysis is a function pass, we need to pass the
-      // current function we are examining to the getAnalysis() call.
-      MemoryDependenceAnalysis &MDA = getAnalysis<MemoryDependenceAnalysis>(*mi);
-      PostDominatorTree &PDT = getAnalysis<PostDominatorTree>(*mi);
+        void getAnalysisUsage(AnalysisUsage &AU) const {
+            // For memory dependence analysis
+            AU.addRequired<AAResultsWrapperPass>();
+            AU.addRequired<MemoryDependenceAnalysis>();
 
-      DataDep.getDataDependencies(*mi, MDA, AA);
+            // For control dependence analysis
+            AU.addRequired<PostDominatorTree>();
 
-      ControlDep.getControlDependencies(*mi, PDT);
+            AU.setPreservesAll();
+        }
 
-    } // end for (module::iterator)
+        void print(raw_ostream &OS, const Module *m = 0) const {
+            // dump the contents of the local Deps_ map
+            OS << "Local Dependence map size: " << DataDep.LocalDeps_.size() << '\n';
+            for (auto i = DataDep.LocalDeps_.begin(); i != DataDep.LocalDeps_.end(); ++i) {
+                assert(i->first && "NULL inst found");
+                OS << "Instruction: " << *(i->first);
+                OS << "\n    has dependence\n";
+                OS << "    with instruction " << *(i->second.DepInst_) << '\n';
+                OS << "    of type " << DataDependence::depTypeToString(i->second.Type_) << '\n';
+            }
+            OS << "Non-Local Dependence map size: " << DataDep.NonLocalDeps_.size() << '\n';
+            for (auto i = DataDep.NonLocalDeps_.begin(); i != DataDep.NonLocalDeps_.end(); ++i) {
+                assert(i->first && "NULL inst found");
+                OS << "Instruction: " << *(i->first)
+                   << "\n    has non local dependence(s) with:\n";
+                for (auto j = i->second.begin(); j != i->second.end(); ++j) {
+                    assert(j->getAddress());
+                    OS << "    Address: " << *(j->getAddress()) << '\n';
+                }
+            }
 
+            // create a dot file for the CDG
+            ControlDep.toDot(std::string(""));
 
-    // Nothing modified in IR
-    return false;
-  }
+            // dump the contents of the control dependencies
+            for (auto i = ControlDep.controlDeps_.begin(),
+                         ei = ControlDep.controlDeps_.end(); i != ei; ++i) {
+                std::set<BasicBlock *> depSet;
+                depSet = i->second;
 
-  void DependenceCheck::print(raw_ostream &OS, const Module *m) const {
-    // dump the contents of the local Deps_ map
-    OS << "Local Dependence map size: " << DataDep.LocalDeps_.size() << '\n';
-    for (auto i = DataDep.LocalDeps_.begin(); i != DataDep.LocalDeps_.end(); ++i) {
-      assert(i->first && "NULL inst found");
-      OS << "Instruction: " << *(i->first); 
-      OS << "\n    has dependence\n";
-      OS << "    with instruction " << *(i->second.DepInst_) << '\n';
-      OS << "    of type " << DataDependence::depTypeToString(i->second.Type_) << '\n';
-    }
-    OS << "Non-Local Dependence map size: " << DataDep.NonLocalDeps_.size() << '\n';
-    for (auto i = DataDep.NonLocalDeps_.begin(); i != DataDep.NonLocalDeps_.end(); ++i) {
-      assert(i->first && "NULL inst found");
-      OS << "Instruction: " << *(i->first) 
-         << "\n    has non local dependence(s) with:\n";
-      for (auto j = i->second.begin(); j != i->second.end(); ++j) {
-        assert(j->getAddress());
-        OS << "    Address: " << *(j->getAddress()) << '\n';
-      }
-    }
-
-    // create a dot file for the CDG
-    ControlDep.toDot(std::string(""));
-
-    // dump the contents of the control dependencies
-    for (auto i = ControlDep.controlDeps_.begin(), 
-        ei = ControlDep.controlDeps_.end(); i != ei; ++i) {
-      std::set<BasicBlock *> depSet;
-      depSet = i->second;
-
-      OS << "BasicBlock: " << *(i->first)
-         << "Is dependent on:\n";
-      for (auto j = depSet.begin(), ej = depSet.end(); j != ej; ++j) {
-        // *j is a pointer to a BasicBlock, **j is a BasicBlock
-        OS << **j << '\n';
-      }
-    }
-  }
-
-  DependenceCheck::DependenceCheck() : ModulePass(ID) { 
-    initializeMemoryDependenceAnalysisPass(*PassRegistry::getPassRegistry());
-  }
-
-  void DependenceCheck::getAnalysisUsage(AnalysisUsage &AU) const {
-    // For memory dependence analysis
-    AU.addRequired<AliasAnalysis>();
-    AU.addRequired<MemoryDependenceAnalysis>();
-
-    // For control dependence analysis
-    AU.addRequired<PostDominatorTree>();
-
-    AU.setPreservesAll();
-  }
-
+                OS << "BasicBlock: " << *(i->first)
+                   << "Is dependent on:\n";
+                for (auto j = depSet.begin(), ej = depSet.end(); j != ej; ++j) {
+                    // *j is a pointer to a BasicBlock, **j is a BasicBlock
+                    OS << **j << '\n';
+                }
+            }
+        }
+    };
 } // end namespace
 
 char DependenceCheck::ID = 0;
-static RegisterPass<DependenceCheck> X("depcheck", "check control and data dependencies of instructions", 
-    true, /* does not modify CFG */
-    true); /* analysis pass */
+static RegisterPass<DependenceCheck> X("dependencecheck", "check control and data dependencies of instructions",
+                                       true, /* does not modify CFG */
+                                       true); /* analysis pass */
 
 // Automatically enable the pass.
 // http://adriansampson.net/blog/clangpass.html
 static void registerDependenceCheck(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new DependenceCheck());
+                                    legacy::PassManagerBase &PM) {
+    PM.add(new DependenceCheck());
 }
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerDependenceCheck);
+
+static RegisterStandardPasses RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible, registerDependenceCheck);
